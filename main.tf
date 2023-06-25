d4d74148dc1e6853f54b1ff0618bcd110e83c716
erog-tech <prikshitthakur0@gmail.com>
2023-06-25 20:55:16 +0200

    tf conf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "erog-tech"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "erog-tech"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "space-beacon"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "space-beacon"
+    }
+  }
+}
