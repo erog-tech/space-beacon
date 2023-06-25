67482870fab4388f5d5c160f485a59ba901f7731
erog-tech <prikshitthakur0@gmail.com>
2023-06-25 22:47:33 +0200

    add tf var

--- eks.tf
+++ eks.tf
@@ -7,25 +7,45 @@ module "eks" {
   vpc_id     = aws_vpc.space_beacon.id
   subnet_ids = [aws_subnet.space_beacon.id]
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 10
-      min_capacity     = 1
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "m6i.large"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
 
-      instance_type = "m5.large"
-      key_name      = "space-beacon"
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
 
-      additional_tags = {
-        Environment = "test"
-        Name        = "eks-worker-node"
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "m6i.large"
+            weighted_capacity = "2"
+          },
+        ]
       }
     }
   }
 
-  manage_aws_auth    = true
-  write_kubeconfig   = true
-  config_output_path = "./"
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
 
   map_users = var.map_users
 }
