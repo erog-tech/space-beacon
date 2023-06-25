58e12a316ad45dbe8367c48dbc8107eada9982f3
erog-tech <prikshitthakur0@gmail.com>
2023-06-25 19:47:02 +0200

    change the name

--- vpc.tf
+++ vpc.tf
@@ -14,49 +14,49 @@ variable "aws_secret_key" {
   type        = string
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "space_beacon" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "main"
+    Name = "space_beacon"
   }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "space_beacon" {
+  vpc_id = aws_vpc.space_beacon.id
 
   tags = {
-    Name = "main"
+    Name = "space_beacon"
   }
 }
 
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "space_beacon" {
+  vpc_id = aws_vpc.space_beacon.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.space_beacon.id
   }
 
   tags = {
-    Name = "main"
+    Name = "space_beacon"
   }
 }
 
-resource "aws_main_route_table_association" "main" {
-  vpc_id         = aws_vpc.main.id
-  route_table_id = aws_route_table.main.id
+resource "aws_space_beacon_route_table_association" "space_beacon" {
+  vpc_id         = aws_vpc.space_beacon.id
+  route_table_id = aws_route_table.space_beacon.id
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "space_beacon" {
+  vpc_id     = aws_vpc.space_beacon.id
   cidr_block = "10.0.1.0/24"
 
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "main"
+    Name = "space_beacon"
   }
 }
