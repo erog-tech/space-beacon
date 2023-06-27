56fb0dc675307abb43c703668c8105868d474f1d
erog-tech <prikshitthakur0@gmail.com>
2023-06-27 18:44:20 +0200

    add the docker conf

--- eks.tf
+++ eks.tf
@@ -1,4 +1,4 @@
-data "aws_eks_cluster" "default" {
+/*data "aws_eks_cluster" "default" {
   name = module.eks.cluster_name
 }
 
@@ -18,7 +18,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.default.token
   }
-}
+}*/
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   version                        = "19.0"
