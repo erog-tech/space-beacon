786011288c2ed1ce9d33de6745819dce612da451
erog-tech <prikshitthakur0@gmail.com>
2023-06-28 00:11:56 +0200

    update workflow

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 terraform {
-  backend "s3" {
-    key    = "prod/terraform.tfstate"
-    region = "eu-central-1"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "erog-tech"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "space-beacon"
+    }
   }
 }
